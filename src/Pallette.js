57bb0ba1c7d126e02cf204f69e6003ac3a77f1f1
ericaustinbarber <ericaustinbarber@ericaustinbarber-ubuntu-64-20.04>
2022-06-01 11:59:45 -0700

    created color pallette, imported colorseed, and created reacted app

---
+++ src/Pallette.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Pallette extends Component {
+  render() {
+    return (
+      <div className="Pallette">
+        {/* Navbar goes here */}
+        <div className="Pallette-colors">{/* bunhc of colors */}</div>
+        {/* footer here eventually */}
+      </div>
+    );
+  }
+}
+
+export default Pallette;
