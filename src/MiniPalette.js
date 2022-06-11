050424f29beea97304b91d699c3b81cafbc0d434
ericaustinbarber <ericaustinbarber@ericaustinbarber-ubuntu-64-20.04>
2022-06-11 14:46:47 -0700

    navigate to palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
   root: {
     background: "white",
+    border: "1px solid black",
     borderRadius: "5px",
     padding: "0.5rem",
     position: "relative",
     overflow: "hidden",
-    "&:hover": {
-      cursor: "pointer",
+    cursor: "pointer",
+    "&:hover svg": {
+      opacity: 1,
     },
   },
   colors: {
+    background: "#dae1e4",
     height: "150px",
     width: "100%",
-    background: "black",
     borderRadius: "5px",
     overflow: "hidden",
   },
@@ -23,7 +26,7 @@ const styles = {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    mergin: "0",
+    margin: "0",
     color: "black",
     paddingTop: "0.5rem",
     fontSize: "1rem",
@@ -33,7 +36,7 @@ const styles = {
     marginLeft: "0.5rem",
     fontSize: "1.5rem",
   },
-  miniColors: {
+  miniColor: {
     height: "25%",
     width: "20%",
     display: "inline-block",
@@ -41,16 +44,30 @@ const styles = {
     position: "relative",
     marginBottom: "-3.5px",
   },
+  deleteIcon: {
+    color: "white",
+    background: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0px",
+    top: "0px",
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0,
+  },
 };
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji, colors } = props;
+  let navigate = useNavigate();
+
+  const { classes, paletteName, emoji, colors, id } = props;
   const miniColorBoxes = colors.map((color) => (
-    <div className={classes.miniColors} style={{ background: color.color }} />
+    <div className={classes.miniColor} style={{ background: color.color }} />
   ));
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={() => navigate(`/palette/${id}`)}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={emoji}>{emoji}</span>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -4,12 +4,27 @@ import { withStyles } from "@material-ui/styles";
 import MiniPalette from "./MiniPalette";
 
 const styles = {
+  "@global": {
+    ".fade-exit": {
+      opacity: 1,
+    },
+    ".fade-exit-active": {
+      opacity: 0,
+      transition: "opacity 500ms ease-out",
+    },
+  },
   root: {
-    background: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    background: "#394bad",
+    // backgroundImage: `url(${bg})`,
+    overflow: "scroll",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -17,17 +32,36 @@ const styles = {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    // [sizes.down("xl")]: {
+    //   width: "80%",
+    // },
+    // [sizes.down("xs")]: {
+    //   width: "75%",
+    // },
   },
   nav: {
     display: "flex",
     width: "100%",
     justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      color: "white",
+    },
   },
   palettes: {
     boxSizing: "border-box",
     width: "100%",
     display: "grid",
-    gridTemplateColumns: "repeat(3,30%)",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "2.5rem",
+    // [sizes.down("md")]: {
+    //   gridTemplateColumns: "repeat(2, 50%)",
+    // },
+    // [sizes.down("xs")]: {
+    //   gridTemplateColumns: "repeat(1, 100%)",
+    //   gridGap: "1.4rem",
+    // },
   },
 };
 
